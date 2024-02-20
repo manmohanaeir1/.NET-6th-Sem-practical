eea818e7f8d74f40be12174a3f8c93fcb270df50
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-02-20 06:47:48 +0545

    for loop

---
+++ loop/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is for loop example ");
+for (int i = 0; i < 10; i++)
+{
+if(i==5)
+{
+    Console.WriteLine($" Is equal to = {i}");
+} 
+{
+    Console.WriteLine(i);
+}
+
+}
+
+
