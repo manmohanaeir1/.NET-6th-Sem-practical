da23f79f54200a6b4586d0a548e88ed187cd64eb
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-02-21 06:38:10 +0545

    this is for int and double example

---
+++ class2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpEg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is for single lline comment 
+
+            /* htis is for 
+            double line comment */
+
+            Console.WriteLine("Hello !");
+
+
+            int i = 30;
+            double d = 3.21;
+
+            Console.WriteLine("THe integer data will be displayed here {0} ", i);
+            Console.WriteLine("THe double data will be displayed here {0} ", d);
+
+        }
+    }
+}
