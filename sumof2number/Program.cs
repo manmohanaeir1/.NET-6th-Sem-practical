b2aeeae67f46f134f99ff5ad668fa8efce27c634
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-02-26 10:38:21 +0545

    sum of n number

---
+++ sumof2number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; // System is a namespace
+
+public class SumOfNumber
+{
+    // Main method which starts the program execution.
+    public static void Main()
+    {
+        int number, sum=0;
+        Console.Write("Enter number: ");
+        number = int.Parse(Console.ReadLine()); 
+        Console.Write("Sum of Number '"+number+ "' : ");
+
+        for (int i = 1; i <= number; i++)
+        {
+            sum = sum + i;
+        }
+        Console.WriteLine(sum);
+
+     }
+}
